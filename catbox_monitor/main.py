a9fd1f6d6deff5cc29ecf266779553ede9c31eb5
Ned Konz <ned@nedkonz.com>
2024-05-22 17:11:17 -0700

    Got load cell working.

--- catbox_monitor/main.py
+++ catbox_monitor/main.py
@@ -1,6 +1,5 @@
 from PyNAU7802 import NAU7802
 from machine import I2C, Pin
-from time import sleep_ms
 
 i2c = I2C(0, scl=Pin(22), sda=Pin(21))
 
@@ -17,7 +16,5 @@ offset = loadcell.zeroOffset
 print(f"Offset: {offset}")
 
 while True:
-    value = loadcell.getAverage(8)
-    print(f"{value:08d}\r", eol="")
-    
-
+    value = loadcell.getWeight(True, 8)
+    print(f"{value:f}\r", end="")
